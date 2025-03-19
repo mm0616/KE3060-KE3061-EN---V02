3289d0cef9dae66d9e58e8add9a16364347b7ece
momo <1668817352@qq.com>
2025-03-19 10:12:55 +0800

    first commit

--- conf.py
+++ conf.py
@@ -23,5 +23,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
-html_static_path = ['_static']
+...
+html_theme = "sphinx_book_theme"
+#html_static_path = ['_static']
+
+html_theme_options = {
+    "repository_url": "https://github.com/mm0616/KE3060-KE3061-EN---V02.git",
+    "use_repository_button": True,
+}
